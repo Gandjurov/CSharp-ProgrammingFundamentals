1814a0d46b711e9d3fe41e5edd708a4745a3c143
Gandjurov <Gandjurov@abv.bg>
2018-09-06 13:11:58 +0300

    Adding Array Exercises

---
+++ 03. Arrays/Arrays_Training/01. LargestCommonEnd/LargestCommonEnd.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._LargestCommonEnd
+{
+    class LargestCommonEnd
+    {
+        static void Main()
+        {
+            string[] words1 = Console.ReadLine().Split(' ').ToArray();
+            string[] words2 = Console.ReadLine().Split(' ').ToArray();
+
+            int leftCount = 0;
+            int rightCount = 0;
+
+            for (int i = 0; i < Math.Min(words1.Length, words2.Length); i++)
+            {
+                if(words1[i] == words2[i])
+                {
+                    leftCount++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            for (int i = 0; i < Math.Min(words1.Length, words2.Length); i++)
+            {
+                if(words1[words1.Length-1-i] == words2[words2.Length-1-i])
+                {
+                    rightCount++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (leftCount > rightCount)
+            {
+                Console.WriteLine(leftCount);
+            }
+            else
+            {
+                Console.WriteLine(rightCount);
+            }
+        
+        }
+    }
+}
+
+// below is faster solution
+
+//string[] array1 = Console.ReadLine()
+//                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+//string[] array2 = Console.ReadLine()
+//    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+//int lengthL = 0;
+//int lengthR = 0;
+//        for (int i = 0; i<Math.Min(array1.Length, array2.Length); i++)
+//        {
+//            if (array1[i] == array2[i])
+//                lengthL++;
+//            if (array1[array1.Length - 1 - i] == array2[array2.Length - 1 - i])
+//                lengthR++;
+//        }
+//        if (lengthL >= lengthR)
+//            Console.WriteLine(lengthL);
+//        else
+//            Console.WriteLine(lengthR);
---
+++ 03. Arrays/Arrays_Training/02. RotateAndSum/RotateAndSum.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._RotateAndSum
+{
+    class RotateAndSum
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int numberOfRotations = int.Parse(Console.ReadLine());
+            int[] sum = new int[numbers.Length];
+
+            for (int i = 0; i < numberOfRotations; i++)
+            {
+                numbers = Rotate(numbers);
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    sum[j] += numbers[j];
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", sum));
+
+            //int[] numbers = Console.ReadLine()
+            //    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            //    .Select(int.Parse)
+            //    .ToArray();
+            //int rotations = int.Parse(Console.ReadLine());
+            //int[] sumRotatedNumbers = new int[numbers.Length];
+
+            //for (int i = 0; i < rotations; i++)
+            //{
+            //    int[] rotatedNumbers = new int[numbers.Length];
+            //    rotatedNumbers[0] = numbers[numbers.Length - 1];
+            //    for (int j = 1; j < numbers.Length; j++)
+            //        rotatedNumbers[j] = numbers[j - 1];
+            //    for (int j = 0; j < numbers.Length; j++)
+            //        sumRotatedNumbers[j] += rotatedNumbers[j];
+            //    numbers = rotatedNumbers;
+            //}
+            //Console.WriteLine(string.Join(" ", sumRotatedNumbers));
+        }
+
+        static int[] Rotate(int[] numbers)
+        {
+            int[] rotated = new int[numbers.Length];
+            int last = numbers[numbers.Length - 1];
+
+            for (int i = 1; i < rotated.Length; i++)
+            {
+                rotated[i] = numbers[i - 1];
+            }
+            rotated[0] = last;
+            return rotated;
+        }
+    }
+}
