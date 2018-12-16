62fca72cbaffd771cfac85ddb2321c658060b7c5
Gandjurov <gandjurov@abv.bg>
2018-12-17 00:51:30 +0200

    add 07.MathPower

---
+++ 02. Methods/07.MathPower/MathPower.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class MathPower
+{
+    static void Main(string[] args)
+    {
+        var baseNumber = double.Parse(Console.ReadLine());
+        var power = int.Parse(Console.ReadLine());
+        Console.WriteLine(Pow(baseNumber, power));
+    }
+
+    public static double Pow(double baseNumber, int power)
+    {
+        double result = 1;
+
+        for (int i = 0; i < power; i++)
+        {
+            result *= baseNumber;
+        }
+
+        return result;
+    }
+}
